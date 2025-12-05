a3ede96efed31d71ddd953a7076600e066f46582
SleepyThree <slimok7@gmail.com>
2025-12-05 12:11:32 +0100

    Polaczenie z internetem i Toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +9,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/sleepythree/retrofit-Jason/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Pytanie>> call = jsonPlaceHolderApi.getPytanie();
+        call.enqueue(
+                new Callback<List<Pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
+                        if (!response.isSuccessful()){
+                            Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        List<Pytanie> pytania = response.body();
+                        Toast.makeText(MainActivity.this,
+                                pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Pytanie>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+
 
     }
 }
